530dc98983bd5b7a5310a3e1556924547d97d1e8
Primoz Skale <skalca@users.noreply.github.com>
2018-04-10 13:36:59 +0200

    Fixed all comments and functions

--- mrz-extractor.js
+++ mrz-extractor.js
@@ -1,17 +1,16 @@
 
 /**
- * 
- * Extracts a MRZ area from a image of a identity document (eg. Passport, Identity card, etc...).
+ * Extracts a MRZ area from an image of an identity document (eg. Passport, Identity card, etc...).
  * 
  * @param {Canvas} canvas - JS object of the canvas where image with MRZ area is painted.
- * @param {boolean} [rotate=false] - Should the image in the canvas be rotated counter-clockwise 90 deg before MRZ area extraction.
+ * @param {boolean} [rotate=false] - Should the image in the canvas be rotated counter-clockwise 90 deg before MRZ area extraction?
  */
 function MrzExtractor(canvas, rotate  = false) {
 	
 	this.canvas = canvas;
 	this.rotate = rotate;
 
-	this.original = null;		
+	this.original = null;	
 	
 	/**
 	 * Main entry method to extract MRZ area from
@@ -20,12 +19,15 @@ function MrzExtractor(canvas, rotate  = false) {
 	 * into the canvas.
 	 */
 	this.extractMRZ = () => {
+		// Original image to process
+		this.original = cv.imread(this.canvas);
+		// Rotate if so directed
 		if (this.rotate) {
 			this.rotateImage90();
 		}
-
-		this.poravnaj();
-		this.mrz()	
+		// Adjust text alignment and find MRZ area
+		this.textAlignAdjustment();
+		return this.findMRZArea();
 	};
 	
 	/**
@@ -34,41 +36,35 @@ function MrzExtractor(canvas, rotate  = false) {
 	 * Set input parameter 'rotate' to true if the image in canvas
 	 * was taken in portrait mode.
 	 */
-	this.rotateImage90 = () => {
-		let src = cv.imread('canvas');
-		let dsize = new cv.Size(src.cols, src.rows);
-		let center = new cv.Point(src.cols / 2, src.rows / 2);				
+	this.rotateImage90 = () => {		
+		let dsize = new cv.Size(original.cols, original.rows);
+		let center = new cv.Point(original.cols / 2, original.rows / 2);				
 		let M = cv.getRotationMatrix2D(center, -90, 1);
-		cv.warpAffine(src, src, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+		cv.warpAffine(original, original, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
 		
-		// Paint rotated image to canvas
-		cv.imshow('canvas', src);
+		// Paint rotated image back to canvas
+		cv.imshow(this.canvas, original);
 
 		dsize.delete();
-		M.delete();
-		src.delete();
+		M.delete();		
 	};
 	
 	/**
 	 * Slightly rotates the image (for a few degress) so that the text 
 	 * is horizontally aligned as possible.
 	 */
-	this.textAlignAdjustment = function() {
+	this.textAlignAdjustment = () => {
 
 		// Returns an angle of Hough line
 		let houghAngle = (p1,p2) => { Math.atan2(p2.y - p1.y, p2.x - p1.x) * (180 / Math.PI); };
 
-		let src = cv.imread('canvas');
-		let dst = new cv.Mat();
+		// Convert the original image to maximum height of 1200px
+		let scaleW = original.size().height / 1200;
+		let dsize = new cv.Size(original.size().width / scaleW, 1200);
+		cv.resize(original, original, dsize, 0, 0, cv.INTER_AREA);
 	
-		// Convert the image to maximum height of 1200px
-		let scaleW = src.size().height / 1200;
-		let dsize = new cv.Size(src.size().width / scaleW, 1200);
-		cv.resize(src, dst, dsize, 0, 0, cv.INTER_AREA);
-	
-		// Kloniraj original
-		original = dst.clone();
-	
+		// Clone original because we do different things to image further on
+		let dst = original.clone();
 		
 		cv.cvtColor(dst, dst, cv.COLOR_RGBA2GRAY, 0);
 		let ksize = new cv.Size(3, 3);
@@ -96,13 +92,13 @@ function MrzExtractor(canvas, rotate  = false) {
 		let lines = new cv.Mat();
 		cv.Canny(dst, dst, 50, 200, 3);
 		cv.HoughLinesP(dst, lines, 1, Math.PI / 180, 10, 100, 10);	
-		let arrRotirajZa = new Array();		
+		let arrRotateForAngle = new Array();		
 		let color = new cv.Scalar(255, 0, 0);		
 		for (let i = 0; i < lines.rows; ++i) {					
 			let startPoint = new cv.Point(lines.data32S[i * 4], lines.data32S[i * 4 + 1]);
 			let endPoint = new cv.Point(lines.data32S[i * 4 + 2], lines.data32S[i * 4 + 3]);					
 			cv.line(dst, startPoint, endPoint, color);			
-			arrRotirajZa.push(houghAngle(startPoint, endPoint));
+			arrRotateForAngle.push(houghAngle(startPoint, endPoint));
 		}
 		lines.delete();
 	
@@ -110,13 +106,13 @@ function MrzExtractor(canvas, rotate  = false) {
 		// Here we count in general which angles are most common.
 		let counts = {};
 		let kotInt = 0;
-		arrRotirajZa.sort();				
-		for(let i=0;i<arrRotirajZa.length; i++) {					
-			kotInt = parseInt(arrRotirajZa[i], 10);
+		arrRotateForAngle.sort();				
+		for(let i=0;i<arrRotateForAngle.length; i++) {					
+			kotInt = parseInt(arrRotateForAngle[i], 10);
 			// Angle is considered only if the difference between int part of
 			// the angle and the angle itself is > 0.001
 			// We do this, so that angles like 0 or 90 deg. fall out
-			if (Math.abs(Math.abs(kotInt) - Math.abs(arrRotirajZa[i])) > 0.001) {
+			if (Math.abs(Math.abs(kotInt) - Math.abs(arrRotateForAngle[i])) > 0.001) {
 				counts[kotInt] = 1 + (counts[kotInt] || 0);
 			}
 		};
@@ -128,63 +124,50 @@ function MrzExtractor(canvas, rotate  = false) {
 				maxKot = Object.keys(counts)[i];
 			}					
 		};
-		let rotirajZa = 0;
-		for(let i=0;i<arrRotirajZa.length; i++) {
-			if (parseInt(arrRotirajZa[i], 10) == maxKot) {
-				rotirajZa = arrRotirajZa[i];
+		let rotateForAngle = 0;
+		for(let i=0;i<arrRotateForAngle.length; i++) {
+			if (parseInt(arrRotateForAngle[i], 10) == maxKot) {
+				rotateForAngle = arrRotateForAngle[i];
 			}
 		}
 	
-		if (rotirajZa < 0) {
-			rotirajZa = 360 + rotirajZa;
+		if (rotateForAngle < 0) {
+			rotateForAngle = 360 + rotateForAngle;
 		}
 	
-		// Make a rotation
+		// Rotate original canvas image
 		let dsize1 = new cv.Size(original.cols, original.rows);
 		let center = new cv.Point(original.cols / 2, original.rows / 2);				
-		let M1 = cv.getRotationMatrix2D(center, rotirajZa, 1);
+		let M1 = cv.getRotationMatrix2D(center, rotateForAngle, 1);
 		cv.warpAffine(original, original, M1, dsize1, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
-		M1.delete();
-	
-		cv.imshow('canvas', original);
+			
+		cv.imshow(this.canvas, original);
 
-		src.delete(); dst.delete(); rectKernel.delete(); sqKernel.delete();
+		M1.delete();		
+		dst.delete(); 
+		rectKernel.delete(); 
+		sqKernel.delete();
 	};
 	
 	/**
 	 * Finds a MRZ area/region and crops it.
 	 */
-	this.mrz = () => {
-						
-		let dst = new cv.Mat();
-	
-		// Jo pretvorimo v največ 1200px višine
-		let scaleW = original.size().height / 1200;
-		let dsize = new cv.Size(original.size().width / scaleW, 1200);
-		cv.resize(original, dst, dsize, 0, 0, cv.INTER_AREA);
-	
-		// Kloniraj original
-		original = dst.clone();
-	
-		// Pretvorimo v Greyscale
+	this.findMRZArea = () => {
+
+		let dst = original.clone(); 
+
 		cv.cvtColor(dst, dst, cv.COLOR_RGBA2GRAY, 0);
-		
-		// Smooth sliko z 3x3 Gaussian
+
 		let ksize = new cv.Size(3, 3);
 		cv.GaussianBlur(dst, dst, ksize, 0, 0);
-		
-		// Naredi Blackhat morfologijo, da identificiraš temna področja na svetlem ozadju				
+
 		let rectKernel = new cv.Mat();
 		rectKernel = cv.getStructuringElement(cv.MORPH_RECT, new cv.Size(16, 10));
 		cv.morphologyEx(dst, dst, cv.MORPH_BLACKHAT, rectKernel);
-	
-		// Izračunaj magnitudo gradienta z uporabo operatorja Scharr/Sobel
+
 		cv.Sobel(dst, dst, cv.CV_8U, 1, 0, 1);
 		cv.convertScaleAbs(dst, dst, 1, 0);
-	
-		//cv.imshow('canvas', dst);
-	
-		// Najdi območja, ki so tekstovno povezana
+
 		cv.morphologyEx(dst, dst, cv.MORPH_CLOSE, rectKernel);
 		cv.threshold(dst, dst, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU);
 		
@@ -225,7 +208,7 @@ function MrzExtractor(canvas, rotate  = false) {
 				if (h < 0) { h = 0; };
 	
 				rect1 = new cv.Rect(x,y,w,h);						
-				original = original.roi(rect1);
+				original = original.roi(rect1);	// Crop MRZ area from original image
 	
 				mrzAreaFound = true;
 	
@@ -237,7 +220,7 @@ function MrzExtractor(canvas, rotate  = false) {
 			return false;
 		}
 	
-		cv.imshow('canvas', original);
+		cv.imshow(this.canvas, original);
 	
 		dst.delete();
 		sqKernel.delete(); rectKernel.delete();
